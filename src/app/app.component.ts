b6ad08d0af6db4cb760d7676280e3ac88ed1a87b
Ryan Gomoto <ryan@cyrusbio.com>
2017-11-11 00:40:01 -0800

    Create tumblr form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  tumblrForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.tumblrForm = this.formBuilder.group({
+      blog: this.formBuilder.control('')
+    });
+  }
 }

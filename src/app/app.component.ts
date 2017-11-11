c64c4f8365b0c6c0edadd78b87fc7c61b9c4347a
Ryan Gomoto <ryan@cyrusbio.com>
2017-11-11 01:45:59 -0800

    Display date, notes, and link of blog posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import * as request from 'request';
-import { InfoResponse } from './tumblr';
+import { InfoResponse, PostsResponse } from './tumblr';
 
 const apiKey = 'u9oKp2z6VfHuyX7mkfX40S2uSfjZpYSKc6EkMWo2F9SbVtM1hS';
 
@@ -11,7 +11,11 @@ interface Blog {
   posts: Post[];
 }
 
-interface Post {}
+interface Post {
+  date: string;  // Date
+  link: string;
+  notes: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -47,10 +51,20 @@ export class AppComponent {
   }
 
   getPosts(): void {
+    if (this.blog === null) {
+      throw new Error('blog must be defined before getting posts');
+    }
     const url = `https://api.tumblr.com/v2/blog/${this.blog.name}/posts?api_key=${apiKey}`;
     request(url, (error, response, body) => {
-      const data: InfoResponse = JSON.parse(body);
-      console.log(data);
+      const data: PostsResponse = JSON.parse(body);
+      // Set blog posts.
+      this.blog.posts = data.response.posts.map((post) => {
+        return <Post> {
+          date: post.date,
+          link: `https://${post.blog_name}.tumblr.com/post/${post.id}`,
+          notes: post.note_count
+        };
+      });
     });
   }
 }
--- src/app/tumblr.d.ts
+++ src/app/tumblr.d.ts
@@ -29,3 +29,75 @@ export interface InfoResponse {
     }
   }
 }
+
+// Posts.
+// Response from api.tumblr.com/v2/blog/{blog}/posts?api_key={key}
+export interface PostsResponse {
+  meta: {
+    status: number;
+    msg: string;
+  }
+  response: {
+    blog: {
+      ask: boolean;
+      ask_anon: boolean;
+      ask_page_title: string;
+      can_subscribe: boolean;
+      description: string;
+      is_adult: boolean;
+      is_nsfw: boolean;
+      is_optout_ads: boolean;
+      name: string;
+      posts: number;
+      reply_conditions: string;
+      share_likes: boolean;
+      subscribed: boolean;
+      title: string;
+      total_posts: number;
+      updated: number;
+      url: string;
+    }
+    posts: TumblrPost[];
+  }
+}
+
+export interface TumblrPost {
+  blog_name: string;
+  can_like: boolean;
+  can_reblog: boolean;
+  can_reply: boolean;
+  can_send_in_message: boolean;
+  caption: string;
+  date: string;
+  display_avatar: boolean;
+  format: string;
+  id: number;
+  image_permalink: string;
+  is_blocks_post_format: boolean;
+  link_url: string;
+  note_count: number;
+  photos: {
+    alt_sizes: TumblrPhoto[];
+    caption: string;
+    original_size: TumblrPhoto;
+  }[]
+  post_url: string;
+  reblog: {
+    comment: string;
+    tree_html: string;
+  }
+  reblog_key: string;
+  short_url: string;
+  slug: string;
+  state: string;
+  summary: string;
+  tags: string[];
+  timestamp: number;
+  type: string;
+}
+
+export interface TumblrPhoto {
+  url: string;
+  width: number;
+  height: number;
+}
